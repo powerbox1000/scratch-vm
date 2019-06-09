4b1d44de053391e8de15896202ca244bf53c11e2
SheepTester <ussa.say@gmail.com>
2019-06-09 13:28:11 -0700

    Build for $(git log --pretty=format:%H -n1)

--- vm.js
+++ vm.js
@@ -1,6 +1,5 @@
 window.ScratchStorage = require('scratch-storage');
-window.NotVirtualMachine = require('..');
+window.NotVirtualMachine = require('..'); // this is because I guess `window.VirtualMachine` is being overwritten elsewhere oh well
 window.ScratchRender = require('scratch-render');
 window.AudioEngine = require('scratch-audio');
 window.ScratchSVGRenderer = require('scratch-svg-renderer');
-
