c46a8fbded33d6168db11df61ad13c11762eab42
powerbox1000 <72870790+powerbox1000@users.noreply.github.com>
2020-11-12 10:50:06 -0500

    Add new keys (beta)

--- src/io/keyboard.js
+++ src/io/keyboard.js
@@ -10,7 +10,20 @@ const KEY_NAME = {
     UP: 'up arrow',
     RIGHT: 'right arrow',
     DOWN: 'down arrow',
-    ENTER: 'enter'
+    ENTER: 'enter',
+    // tw: extra keys
+    BACKSPACE: 'backspace',
+    DELETE: 'delete',
+    SHIFT: 'shift',
+    CAPS_LOCK: 'caps lock',
+    SCROLL_LOCK: 'scroll lock',
+    CONTROL: 'control',
+    ESCAPE: 'escape',
+    INSERT: 'insert',
+    HOME: 'home',
+    END: 'end',
+    PAGE_UP: 'page up',
+    PAGE_DOWN: 'page down'
 };
 
 /**
@@ -37,6 +50,8 @@ class Keyboard {
          * @type{!Runtime}
          */
         this.runtime = runtime;
+        // tw: track last pressed key
+        this.lastKeyPressed = '';
     }
 
     /**
@@ -58,8 +73,26 @@ class Keyboard {
         case 'Down':
         case 'ArrowDown': return KEY_NAME.DOWN;
         case 'Enter': return KEY_NAME.ENTER;
+        // tw: extra keys
+        case 'Backspace': return KEY_NAME.BACKSPACE;
+        case 'Delete': return KEY_NAME.DELETE;
+        case 'Shift': return KEY_NAME.SHIFT;
+        case 'CapsLock': return KEY_NAME.CAPS_LOCK;
+        case 'ScrollLock': return KEY_NAME.SCROLL_LOCK;
+        case 'Control': return KEY_NAME.CONTROL;
+        case 'Escape': return KEY_NAME.ESCAPE;
+        case 'Insert': return KEY_NAME.INSERT;
+        case 'Home': return KEY_NAME.HOME;
+        case 'End': return KEY_NAME.END;
+        case 'PageUp': return KEY_NAME.PAGE_UP;
+        case 'PageDown': return KEY_NAME.PAGE_DOWN;
         }
-        return keyString.toUpperCase();
+        // Ignore modifier keys
+        if (keyString.length > 1) {
+            return '';
+        }
+        // tw: toUpperCase() happens later. We need to track key case.
+        return keyString;
     }
 
     /**
@@ -91,9 +124,23 @@ class Keyboard {
             return keyArg;
         }
 
+        // Use only the first character.
+        if (keyArg.length > 1) {
+            keyArg = keyArg[0];
+        }
+
         // Check for the space character.
         if (keyArg === ' ') {
             return KEY_NAME.SPACE;
+        }
+        // tw: support Scratch 2 hacked blocks
+        // There are more hacked blocks but most of them get mangled by Scratch 2 -> Scratch 3 conversion
+        if (keyArg === '\r') {
+            // this probably belongs upstream
+            return KEY_NAME.ENTER;
+        }
+        if (keyArg === '\u001b') {
+            return KEY_NAME.ESCAPE;
         }
 
         return keyArg.toUpperCase();
@@ -105,10 +152,14 @@ class Keyboard {
      */
     postData (data) {
         if (!data.key) return;
-        const scratchKey = this._keyStringToScratchKey(data.key);
+        // tw: convert single letter keys to uppercase because of changes in _keyStringToScratchKey
+        const scratchKeyCased = this._keyStringToScratchKey(data.key);
+        const scratchKey = scratchKeyCased.length === 1 ? scratchKeyCased.toUpperCase() : scratchKeyCased;
         if (scratchKey === '') return;
         const index = this._keysPressed.indexOf(scratchKey);
         if (data.isDown) {
+            // tw: track last pressed key
+            this.lastKeyPressed = scratchKeyCased;
             this.runtime.emit('KEY_PRESSED', scratchKey);
             // If not already present, add to the list.
             if (index < 0) {
@@ -131,6 +182,11 @@ class Keyboard {
         }
         const scratchKey = this._keyArgToScratchKey(keyArg);
         return this._keysPressed.indexOf(scratchKey) > -1;
+    }
+
+    // tw: expose last pressed key
+    getLastKeyPressed () {
+        return this.lastKeyPressed;
     }
 }
 
